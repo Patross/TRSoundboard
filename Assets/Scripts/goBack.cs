90ae9d2e791b0c1b30afc06e79af4ed0a9f1bcf2
PatrossDev <contactpatross@gmail.com>
2018-12-03 16:23:21 +0000

    back buttons added

---
+++ Assets/Scripts/goBack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goBack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GoBack()
+    {
+        //Gets current scene so we  can grab the name
+        Scene scene = SceneManager.GetActiveScene();
+
+        //Compares the current scene name and redirects accordingly
+        if (scene.name == "Scream Sounds") { SceneManager.LoadScene("Soundboard"); }
+        if (scene.name == "Soundboard") { SceneManager.LoadScene("MainMenu"); }
+
+    }
+
+}
